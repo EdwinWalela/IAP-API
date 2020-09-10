87f9a9b24a100fe23707372b2c1dc7b387e321eb
edwinwalela <edwinwalela@gmail.com>
2020-09-10 17:07:12 +0300

    home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    IAP API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a target="_blank" href="https://github.com/EdwinWalela/IAP-API">GitHub</a>
+                    <a target="_blank" href="https://documenter.getpostman.com/view/4654837/T1DjkL48?version=latest">Docs</a>
                 </div>
             </div>
         </div>
